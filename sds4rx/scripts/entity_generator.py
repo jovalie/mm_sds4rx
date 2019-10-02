d0212b30315bb330aefc391114ba055e8ed35340
jovalie <zheng673@umn.edu>
2019-10-02 13:30:11 -0500

    add gazetters+mappings for brand, condition, and dosages. updated entity_generator script to match

--- sds4rx/scripts/entity_generator.py
+++ sds4rx/scripts/entity_generator.py
@@ -1,44 +1,105 @@
 import json
 import rxnorm
 
-ENTITY = "brand"
-
 RX_KB = "50_common_rx.json"
 ING_JSON = "50_common_ing.json"
 
 MAPPING_FORMAT = {
     "kb_index_name": "medication",
-	"kb_field_name": ENTITY,
+    "kb_field_name": "",
     "entities": []
 }
 
-G_FILE = ENTITY+"_gazetteer.txt"
-M_FILE = ENTITY+"_mapping.json"
+G_FILE = "_gazetteer.txt"
+M_FILE = "_mapping.json"
 
 def _collect_entities(db, key):    
     cnames = []
     entities = []
     for entry in db:
         e = entry[key]
+
         if e not in cnames:
             print(e)
             cnames.append(e)
             entity = {
                 "whitelist": [],
                 "cname": e,
-                "id": rxnorm.string_to_rxcui(e)[0] #FIXME: connection 104 error?
+                "id": rxnorm.string_to_rxcui(e)[0]
             }
+            
             entities.append(entity)
 
     mapping = MAPPING_FORMAT
+    mapping["kb_field_name"] = key
     mapping["entities"] = entities
 
-    with open(G_FILE, 'w+') as f:
+    with open(key+G_FILE, 'w+') as f:
         f.write('\n'.join(cnames))
  
-    with open(M_FILE, 'w+') as f:
+    with open(key+M_FILE, 'w+') as f:
         json.dump(mapping, f)
     
+def _collect_condition_entities(db, key='condition'):
+    '''
+    '''
+    cnames = []
+    entities = []
+    for entry in db:
+        print(entry)
+        if key in entry.keys():
+            en = entry[key]
+
+            for e in en:
+                if e not in cnames:
+                    print(e)
+                    cnames.append(e)
+                    entity = {
+                        "whitelist": [],
+                        "cname": e,
+                        "id": e
+                    }
+                    entities.append(entity)
+
+    mapping = MAPPING_FORMAT
+    mapping["kb_field_name"] = key
+    mapping["entities"] = entities
+
+    with open(key+G_FILE, 'w+') as f:
+        f.write('\n'.join(cnames))
+ 
+    with open(key+M_FILE, 'w+') as f:
+        json.dump(mapping, f)
+
+def _collect_dosage_entities(db, key='dosage'):
+    '''
+    '''
+    cnames = []
+    entities = []
+    for entry in db:
+        ings = entry['ingredients']
+
+        for ing in ings:
+            e = ing['dosage']
+            if e not in cnames:
+                print(e)
+                cnames.append(e)
+                entity = {
+                    "whitelist": [],
+                    "cname": e,
+                    "id": e
+                }
+                entities.append(entity)
+
+    mapping = MAPPING_FORMAT
+    mapping["kb_field_name"] = key
+    mapping["entities"] = entities
+
+    with open(key+G_FILE, 'w+') as f:
+        f.write('\n'.join(cnames))
+ 
+    with open(key+M_FILE, 'w+') as f:
+        json.dump(mapping, f)
 
 if __name__ == "__main__":
     with open(RX_KB, 'r') as f:
@@ -47,4 +108,6 @@ if __name__ == "__main__":
     with open(ING_JSON, 'r') as f:
         ing_db = json.load(f)
 
-    _collect_entities(rx_kb, "brand")
+    #_collect_entities(rx_kb, "brand")
+    _collect_dosage_entities(rx_kb, "dosage")
+    # _collect_condition_entires(ing_db)
